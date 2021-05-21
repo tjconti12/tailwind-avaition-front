d5e6bc9413a3ae1dcee14d3210265826d09872ab
tjconti12 <tjconti12@gmail.com>
2021-05-21 02:47:08 -0400

    add logo and update favicon

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,14 @@
-
+import logo from '../../Images/tailwindLogo.png';
 
 const Banner = () => {
     return (
         <div className="mb-28">
-            <h1 className="text-7xl text-center">TAILWIND AVIATION</h1>
+            <div className="flex w-1/3 m-auto justify-evenly items-center">
+                <h1 className="text-7xl text-center">TAILWINDS AVIATION</h1>
+                <div className="w-28">
+                    <img className="w-full" src={logo} alt="tailwind aviation logo" />
+                </div>
+            </div>
             <p className="text-3xl text-center m-5">airplane rental</p>
         </div>
     )
